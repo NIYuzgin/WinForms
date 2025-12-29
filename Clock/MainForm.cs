a1f24e1f1930c70fb96f14f877aa6fd3ac342ad6
Николай Немов <prost91@outlook.com>
2025-12-29 21:42:29 +0300

    Fontfamily Load DONE TODO: FontSize

--- Clock/MainForm.cs
+++ Clock/MainForm.cs
@@ -71,7 +71,7 @@ namespace Clock
 			writer.WriteLine(labelTime.BackColor.ToArgb());
 			writer.WriteLine(labelTime.ForeColor.ToArgb());
 			
-			writer.WriteLine(labelTime.Font.Name);
+			writer.WriteLine(fontDialog.Filename);
 
 			writer.Close();
 
@@ -102,6 +102,10 @@ namespace Clock
 
 				labelTime.BackColor = backgroundColorDialog.Color = Color.FromArgb(Convert.ToInt32(Convert.ToInt32(reader.ReadLine())));
 				labelTime.ForeColor = foregroundColorDialog.Color = Color.FromArgb(Convert.ToInt32(Convert.ToInt32(reader.ReadLine())));
+
+				fontDialog = new FontDialog(reader.ReadLine());
+				labelTime.Font = fontDialog.Font;
+
 
 				reader.Close();
 			}
